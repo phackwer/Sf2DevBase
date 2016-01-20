2d6b5fbe90cdc151181163e2b88b29424c03a115
Pablo Sanchez <phackwer@gmail.com>
2016-01-20 11:59:41 -0200

    [UPD] - Bundles adicionados no AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new SanSIS\CrudBundle\CrudBundle(),
+            new SanSIS\BizlayBundle\BizlayBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -24,6 +26,8 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new SanSIS\DevelBundle\DevelBundle();
+            $bundles[] = new Knp\JsonSchemaBundle\KnpJsonSchemaBundle();
         }
 
         return $bundles;
